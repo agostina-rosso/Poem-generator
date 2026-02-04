faf1e2cf0ea25bbc66146e3cc5a5f5a19393e0db
agostina-rosso <agostina.partenopea@gmail.com>
2026-02-04 12:44:06 +0100

    Updated index

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7e43t102ob02c30d9fabf0c6b85d4a1a";
   let context =
-    "You are a world class poet and you are very good at writing short poems. Your mission is to create a four-line poem in basic HTML and separate each line with a <br/>. Make sure to follow the users instructions. Sign the poem with 'SheCodes AI' in bold";
+    "You are a poet and you are very good at writing short poems. Your mission is to create a four-line poem in basic HTML and separate each line with a <br>. Make sure to follow the users instructions. Sign the poem with 'SheCodes AI' in bold";
   let prompt = `User instructions: Generate a short poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
