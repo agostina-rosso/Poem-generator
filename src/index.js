11be8d8afecf0efb9183bf30a01788897bfe55fe
agostina-rosso <agostina.partenopea@gmail.com>
2026-02-05 11:05:55 +0100

    Updated instructions

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7e43t102ob02c30d9fabf0c6b85d4a1a";
   let context =
-    "You are a poet and you are very good at writing short poems. Your mission is to create a four-line poem in basic HTML and separate each line with a <br>. Make sure to follow the users instructions. Sign the poem with 'SheCodes AI' in bold";
+    "You are a poet and you are very good at writing short poems. Your mission is to create a four-line poem in basic html format and separate each line with a <br>. Make sure to follow the users instructions. Sign the poem with 'SheCodes AI' in bold";
   let prompt = `User instructions: Generate a short poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
